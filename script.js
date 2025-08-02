5cb3c141a6f89bf40f8d63fc9ff18cffc09adbdf
Subham_Sharrma <subhamsharrma@gmail.com>
2025-08-02 10:09:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+const form = document.getElementById("form");
+const url = document.getElementById("url");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const year = document.getElementById("year").value.trim();
+
+  let query = [];
+
+  if (name !== "") query.push("name=" + encodeURIComponent(name));
+  if (year !== "") query.push("year=" + encodeURIComponent(year));
+
+  url.textContent = "https://localhost:8080/" + (query.length ? "?" + query.join("&") : "");
+});
